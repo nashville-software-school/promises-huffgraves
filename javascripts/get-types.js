c4edd4da2f37f7a7f58a3402fa63f92be852269a
Kirk Graves <graves.kirk@gmail.com>
2015-09-26 13:24:51 -0500

    created books and types modules and began logic for promises.js

---
+++ javascripts/get-types.js
@@ -0,0 +1,35 @@
+define(function(require) {
+	var Q = require("q");
+  
+
+  return {
+
+		getTypes: function() {
+			var deferred = Q.defer();
+
+			$.ajax({url: "https://nss-book-store.firebaseio.com/booktypes.json"})
+			// XHR was successful
+			.done(function(firebaseTypes) {
+				// Now we can resolve the promise and send the data
+				firebaseTypes = Object.keys(firebaseTypes).map(key => firebaseTypes[key]);
+				deferred.resolve(firebaseTypes);	
+			})	
+		
+
+			// XHR failed for some reason
+		  .fail(function(xhr, status, error) {
+		    // Since the call failed, we have to reject the promise
+		    deferred.reject(error);
+		  })
+
+			return deferred.promise; 
+		}
+
+	
+	}
+
+})
+
+
+
+		
