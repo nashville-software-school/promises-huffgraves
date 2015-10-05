815a7ed2dd734e0fa6c712139e56bfae61c5528e
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-10-05 12:17:56 -0500

    working.  Now working on the filter bonus requirements and bootstrap

---
+++ javascripts/get-types.js
@@ -0,0 +1,43 @@
+
+
+
+define(function(require) {
+	var Q = require("q");
+	var _= require("lodash");
+  
+
+  return {
+
+        getBookTypes: function() {
+            var deferred = Q.defer();
+
+            $.ajax({url: "https://nss-book-store.firebaseio.com/booktypes.json"})
+            // XHR was successful
+            .done(function(firebaseTypes) {
+                // Now we can resolve the promise and send the data
+                // firebaseTypes = Object.keys(firebaseTypes).map(key => firebaseTypes[key]);
+                deferred.resolve(firebaseTypes);        
+            })
+
+            // XHR failed for some reason
+          .fail(function(xhr, status, error) {
+            // Since the call failed, we have to reject the promise
+            deferred.reject(error);
+          })
+
+          return deferred.promise;
+        }
+    }
+
+})
+
+
+
+
+
+// =======below is copied from promises.js========
+
+ // return {
+ //    load: function(fn) {
+ //      // This XHR should be in its own require module, not here
+ //      $.ajax("https://nss-book-store.firebaseio.com/booktypes.json").done(function(types) {
--- javascripts/promises.js
+++ javascripts/promises.js
@@ -1,25 +1,4 @@
-requirejs.config({
-  baseUrl: './javascripts',
-  paths: {
-    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
-    'lodash': '../lib/bower_components/lodash/lodash.min',
-    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
-  },
-  shim: {
-    'bootstrap': ['jquery']
-  }
-});
 
-requirejs(
-  ["jquery", "hbs", "bootstrap", "get-books"], 
-  function($, Handlebars, bootstrap, books) {
-
-    books.load(function(bookArray) {
-      require(['hbs!../templates/books'], function(bookTpl) {
-        $("#bookList").html(bookTpl({ books:bookArray }));
-      });
-    });
 
     /* Here's some pseudo-code for how it should look once you
        start using promises
@@ -41,5 +20,92 @@ requirejs(
       })
      */
 
+  // ====================
+
+  requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q'
+  },
+  shim: {
+    'bootstrap': ['jquery']
   }
-);
+});
+
+requirejs(
+  ["jquery", "q", "hbs", "bootstrap", "get-books", "get-types"], 
+  function($, q, Handlebars, bootstrap, getBooks, getBookTypes) {
+
+    // books.getBooks(function(bookArray) {
+    //   require(['hbs!../templates/books'], function(bookTpl) {
+    //     $("#bookList").html(bookTpl({ books:bookArray }));
+    //   });
+    // }); 
+    var firebaseTypes;
+
+
+    getBookTypes.getBookTypes()
+      .then(function(types){
+        firebaseTypes = types;
+        console.log(types);
+        console.log(firebaseTypes);
+        // console.log("getBooks", getBooks);
+        return getBooks.getBooks(types);
+
+      })
+      .then(function(books) {
+        types = firebaseTypes;
+        //below is working -object of books
+        console.log(books);
+
+
+        types = Object.keys( firebaseTypes ).map(key=> types[ key ]);
+        books = Object.keys( books ).map(key => books [ key ]);
+        // console.log("test");
+
+        var books = books.map(book=> {
+          book.type=_.find(types, { id:book.booktype}).label;
+          return book;
+        })
+
+        require(['hbs!../templates/books'], function(bookTpl) {
+          $("#bookList").append(bookTpl({ books: books }));
+        });
+
+    });
+
+//         getbooks.getBooks()
+//       .then(function(firebaseBooks) {
+//         console.log('firebaseBooks', firebaseBooks);
+//         return types.getBookTypes(firebaseBooks);
+//        })
+//       .then(function(firebaseTypes) {
+//           firebaseBooks = firebaseBooks.map(book => {
+//           book.type = _.find(firebaseTypes, { id:book.booktype }).label;
+//           console.log('book', book);
+//           return book;
+//         });
+
+//         console.log('firebaseTypes', firebaseTypes);
+//         console.log('firebaseBooks', firebaseBooks);
+      
+//       })   
+}) 
+  // =======================================
+// 
+// /*
+//   Now we use those Promises to describe the order of execution, 
+//   and how data flows between each one.
+//  */
+// firstXHR()
+//   .then(function(data1) {
+//     return secondXHR(data1);
+//   })
+//   .then(function(data2) {
+//     return thirdXHR(data2);
+//   })
+//   .done();
